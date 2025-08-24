d13be22865de34c114666bdaa758d375a69f2c32
arjunthazhath2001 <arjunta32@gmail.com>
2025-08-24 09:10:09 +0530

    ok

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const {app,BrowserWindow} = require('electron')
+const path = require('node:path')
+
 
 function createWindow(){
     console.log("creating a new window")
     const win= new BrowserWindow({
         width:800,
-        height: 600
+        height: 600,
+        webPreferences:{
+            preload: path.join(__dirname,'preload.js')
+        }
     })
 
     win.loadFile('index.html')
--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
-const {ContextBridge, contextBridge, ipcRenderer}= require('electron')
+const {contextBridge, ipcRenderer}= require('electron')
 
 
 contextBridge.exposeInMainWorld('api',{
     chat: async(prompt)=>{console.log("call to LLM")},
-    docs: async()=>{console.log("fetch from s3")}
+    docs: async()=>{console.log("fetch from s3")},
+    profile: async()=>{console.log("profile info")},
 })
